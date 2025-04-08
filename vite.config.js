0d10b30ebee9396be5fddecce8a669fb17bd7410
Dhananjay Gupta <82753264+dhan2501@users.noreply.github.com>
2025-04-08 12:25:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../staticfiles/frontend/dev', // Make sure this path is correct
+    emptyOutDir: true,
+  },
+  base: '/',
 })
